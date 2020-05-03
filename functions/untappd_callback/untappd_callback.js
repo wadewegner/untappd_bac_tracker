5ba85e872d503c3e67a545b9829209dcd874626e
Wade Wegner <wade.wegner@gmail.com>
2020-05-03 16:28:39 -0700

    redirect

--- functions/untappd_callback/untappd_callback.js
+++ functions/untappd_callback/untappd_callback.js
@@ -5,18 +5,17 @@ exports.handler = async (event, context) => {
     const client_id = process.env.UNTAPPD_CLIENT_ID
     const client_secret = process.env.UNTAPPD_CLIENT_SECRET
     const redirect_url = process.env.UNTAPPD_REDIRECT_URL
-    const auth_url = `https://untappd.com/oauth/authorize/?client_id=${client_id}&client_secret==${client_secret}&response_type=code&redirect_url=${redirect_url}&code=${code}`
+    const auth_url = `https://untappd.com/oauth/authorize/?client_id=${client_id}&client_secret=${client_secret}&response_type=code&redirect_url=${redirect_url}&code=${code}`
 
     return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `${auth_url}` })
+      statusCode: 301,
+      location: auth_url
+      // body: JSON.stringify({ message: `${auth_url}` })
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
 
-      // https://untappd.com/oauth/authorize/?client_id=CLIENTID&client_secret=CLIENTSECRET&response_type=code&redirect_url=REDIRECT_URL&code=CODE
-
-
+      // {"meta":{"http_code":200},"response":{"access_token":"DFC11DF925CA3630423928CDCCBBE6583E2183A2"}}
     }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
