d5e973ae6df10eb7758570103c4d172eae149d41
Wade Wegner <wade.wegner@gmail.com>
2020-05-03 17:18:48 -0700

    debug

--- functions/untappd_callback/untappd_callback.js
+++ functions/untappd_callback/untappd_callback.js
@@ -1,37 +1,31 @@
-const request = require('request')
+const request = require("request");
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-    const code = event.queryStringParameters.code || 'World'
-    const client_id = process.env.UNTAPPD_CLIENT_ID
-    const client_secret = process.env.UNTAPPD_CLIENT_SECRET
-    const redirect_url = process.env.UNTAPPD_REDIRECT_URL
-    const auth_url = `https://untappd.com/oauth/authorize/?client_id=${client_id}&client_secret=${client_secret}&response_type=code&redirect_url=${redirect_url}&code=${code}`
+    const code = event.queryStringParameters.code || "World";
+    const client_id = process.env.UNTAPPD_CLIENT_ID;
+    const client_secret = process.env.UNTAPPD_CLIENT_SECRET;
+    const redirect_url = process.env.UNTAPPD_REDIRECT_URL;
+    const auth_url = `https://untappd.com/oauth/authorize/?client_id=${client_id}&client_secret=${client_secret}&response_type=code&redirect_url=${redirect_url}&code=${code}`;
 
-    let response_message = 'default'
+    let response_message = "default";
 
-    request(auth_url, function (error, response, body) {
-
+    await request(auth_url, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         console.log(`success: ${body}`);
-        response_message = body
-
+        response_message = body;
       } else {
-        console.log(`error: ${error}`)
-        response_message = error
+        console.log(`error: ${error}`);
+        response_message = error;
       }
-      
-        
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: `${response_message}` })
-      }
-
     });
-    
 
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `${response_message}` }),
+    };
   } catch (err) {
-    return { statusCode: 500, body: err.toString() }
+    return { statusCode: 500, body: err.toString() };
   }
-}
+};
